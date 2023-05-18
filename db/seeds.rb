8111002f8b8538010d803bd9bc31d35d3d27ecdf
yetemegn-telaye <lemma19yet@gmail.com>
2023-05-18 16:14:08 +0300

    Add test data for location

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+cities = ["New York", "Los Angeles", "Chicago", 
+"Houston", "Phoenix", "Philadelphia", "San Antonio"]
+cities.each do |city|
+    Location.create(name: city)
+end
+ 
