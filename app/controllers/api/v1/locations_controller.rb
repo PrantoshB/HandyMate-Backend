cdb69f917e5df8b6953ad281152f39cca209e5c9
PrantoshB <pbiswas1007@gmail.com>
2023-05-19 17:12:39 +0530

    Add Create Method for Locations

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -3,4 +3,20 @@ class Api::V1::LocationsController < ApplicationController
     @locations = Location.all
     render json: @locations
   end
+
+  def create
+    @location = Location.new(location_params)
+
+    if @location.save
+      render json: @location, status: 200
+    else
+      render json: {
+        error: "Error creating location..."
+      }
+    end
+  end
+
+  def location_params
+    params.require(:location).permit(:name)
+  end
 end
