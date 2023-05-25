8aa3fcc97f16ab90bee0e00fb2c1a0bcdfbf5f8d
yetemegn-telaye <lemma19yet@gmail.com>
2023-05-25 13:25:31 +0300

    Update users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,15 +1,15 @@
 class Api::V1::UsersController < ApplicationController
-  def index
-    @users = User.all
-    render json: @users
-  end
+  # def index
+  #   @users = User.all
+  #   render json: @users
+  # end
 
-  def show
-    @user = User.find(params[:id])
-    @reservations = @user.reservations
-  end
+  # def show
+  #   @user = User.find(params[:id])
+  #   @reservations = @user.reservations
+  # end
 
-  def new
-    @user = User.new
-  end
+  # def new
+  #   @user = User.new
+  # end
 end
