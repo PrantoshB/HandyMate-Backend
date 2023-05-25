6439a5ff4f70143a4f371fa1e8867105a43f01f0
Jacinta Ngwu <jacintangwu9900@gmail.com>
2023-05-25 11:59:53 +0000

    Add user serializer

---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,7 @@
+class UserSerializer
+  include JSONAPI::Serializer
+  attributes :id, :email, :role
+  attribute :created_date do |user|
+    user && user.created_at.strftime("%d/%m/%Y")
+  end
+end
