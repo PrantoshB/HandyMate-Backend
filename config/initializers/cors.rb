f9266fa3a42e4f5f9359f3d8f0645bcecdd2262f
haliljon <juraboevhaliljon@gmail.com>
2023-05-25 15:35:16 +0500

    Update cors settings

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,12 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'localhost:3001'
-      resource '*',
+      origins 'http://localhost:3001'
+      resource(
+        '*',
         headers: :any,
-        methods: [:get, :post, :put, :patch, :delete, :options, :head],
-        credentials: true
+        expose: ['access-token', 'expiry', 'token-type',"Authorization"],
+        methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      )
     end
 end
   
