bdf23392cd77636b00811f50d2800ba33433a652
Jacinta Ngwu <jacintangwu9900@gmail.com>
2023-05-25 11:53:14 +0000

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-# cities = ["New York", "Los Angeles", "Chicago", 
-# "Houston", "Phoenix", "Philadelphia", "San Antonio"]
+
 # cities.each do |city|
 #     Location.create(name: city)
 # end
