7753f365518aa5ba2526ed7fd41e0b2cfe104358
yetemegn-telaye <lemma19yet@gmail.com>
2023-05-29 01:25:43 +0300

    Add test to check if location object created successfully

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -4,4 +4,7 @@ RSpec.describe Location, type: :model do
     subject do
         @location = Location.new(name: "Raiganj")
     end
+    it "should be valid" do
+        expect(subject).to be_valid
+    end
 end
