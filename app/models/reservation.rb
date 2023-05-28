83c0e3dd09a3b6830c3a1db42b8ee14e556993c4
PrantoshB <pbiswas1007@gmail.com>
2023-05-28 20:30:10 +0530

    Setup spec branch with rspec

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,7 @@ class Reservation < ApplicationRecord
   belongs_to :location
   belongs_to :user
   belongs_to :service
+
+  validates :start_date, presence: true
+  validates :end_date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
 
   has_many :reservations
+
+  validates :full_name, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
