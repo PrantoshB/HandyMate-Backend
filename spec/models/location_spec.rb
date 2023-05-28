598be7e05f3d1448ec1e31d69d1bcdd2a893a98e
yetemegn-telaye <lemma19yet@gmail.com>
2023-05-29 01:35:04 +0300

    Add test to accept null value for location

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -7,4 +7,8 @@ RSpec.describe Location, type: :model do
     it "should be valid" do
         expect(subject).to be_valid
     end
+    it "should be invalid without a name" do
+        subject.name = nil
+        expect(subject).to_not be_valid
+    end
 end
