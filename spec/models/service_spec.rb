a7f92ca0ae42e3bd42744434e0546fdbf099148b
Utsargo Roy <royutsargo@gmail.com>
2023-05-29 10:10:37 +0600

    Add unit test for validity of character length of name

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -1,36 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Service, type: :model do
-   subject do
-    @service = Service.new(name: "Electrician", details: "blah blah blah blah blah blah blah blah", price: 1233, duration: 3, image: "https://demo.com/image.jpg")
-   end
+  subject do
+    @service = Service.new(name: 'Electrician', details: 'blah blah blah blah blah blah blah blah', price: 1233,
+                           duration: 3, image: 'https://demo.com/image.jpg')
+  end
 
-   it "should be valid" do
-        expect(subject).to be_valid
-    end
-    
-    it "should be invalid without a name" do
-        subject.name = nil
-        expect(subject).to_not be_valid
-    end
+  it 'should be valid' do
+    expect(subject).to be_valid
+  end
 
-    it "should be invalid without details" do
-        subject.details = nil
-        expect(subject).to_not be_valid
-    end
+  it 'should be invalid without a name' do
+    subject.name = nil
+    expect(subject).to_not be_valid
+  end
 
-    it "should be invalid without price" do
-        subject.price = nil
-        expect(subject).to_not be_valid
-    end
+  it 'should be invalid without details' do
+    subject.details = nil
+    expect(subject).to_not be_valid
+  end
 
-    it "should be invalid without duration" do
-        subject.duration = nil
-        expect(subject).to_not be_valid
-    end
-    
-    it "should be invalid without image" do
-        subject.image = nil
-        expect(subject).to_not be_valid
-    end
+  it 'should be invalid without price' do
+    subject.price = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should be invalid without duration' do
+    subject.duration = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'should be invalid without image' do
+    subject.image = nil
+    expect(subject).to_not be_valid
+  end
+  it 'should be invalid with a name less than 3 characters' do
+    subject.name = 'ab'
+    expect(subject).to_not be_valid
+  end
 end
