4e4547f67f85310169b9a4d13654678ca7633691
Utsargo Roy <royutsargo@gmail.com>
2023-05-29 10:08:56 +0600

    Add unit test for validity without image

--- spec/models/service_spec.rb
+++ spec/models/service_spec.rb
@@ -23,9 +23,14 @@ RSpec.describe Service, type: :model do
         subject.price = nil
         expect(subject).to_not be_valid
     end
-    
+
     it "should be invalid without duration" do
         subject.duration = nil
+        expect(subject).to_not be_valid
+    end
+    
+    it "should be invalid without image" do
+        subject.image = nil
         expect(subject).to_not be_valid
     end
 end
