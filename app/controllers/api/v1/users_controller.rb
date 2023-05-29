957a7cbfb88a9b06eda01543065613dd3b1e92b9
Utsargo Roy <royutsargo@gmail.com>
2023-05-29 13:05:40 +0600

    Generate API documentation with swagger

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,13 +3,4 @@ class Api::V1::UsersController < ApplicationController
     @users = User.all
     render json: @users
   end
-
-  # def show
-  #   @user = User.find(params[:id])
-  #   @reservations = @user.reservations
-  # end
-
-  # def new
-  #   @user = User.new
-  # end
 end
